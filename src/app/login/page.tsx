8a396de10cb7bd0692579a88dca8b40840858a57
pradhan-not-found <souradeeppradhan7@gmail.com>
2026-08-11 18:58:41 +0530

    Update footer text to 'for devs by devs.'

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -255,7 +255,7 @@ function FooterText({ light = false }: { light?: boolean }) {
       <svg width="12" height="12" viewBox="0 0 24 24" fill="#c0605a" xmlns="http://www.w3.org/2000/svg" style={{ display: 'inline', flexShrink: 0, marginBottom: '-1px' }} aria-label="love">
         <path d="M12 21.593c-5.63-5.539-11-10.297-11-14.402 0-3.791 3.068-5.191 5.281-5.191 1.312 0 4.151.501 5.719 4.457 1.59-3.968 4.464-4.447 5.726-4.447 2.54 0 5.274 1.621 5.274 5.181 0 4.069-5.136 8.625-11 14.402z"/>
       </svg>
-      <span>by Fantastic 4</span>
+      <span>for devs by devs.</span>
     </p>
   );
 }
