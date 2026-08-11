64334b8261a530ef0a578916e7779d0da7d5f079
pradhan-not-found <souradeeppradhan7@gmail.com>
2026-08-11 17:21:41 +0530

    Fix footer image URL regression and update company name/designer attribution

---
+++ fix_company_name.js
@@ -0,0 +1,7 @@
+﻿const fs = require('fs');
+let page = fs.readFileSync('src/app/page.tsx', 'utf8');
+
+page = page.replace(/The General Intelligence Company Of New York/g, 'Fantastic 4');
+
+fs.writeFileSync('src/app/page.tsx', page);
+console.log("Changed company name to Fantastic 4.");
