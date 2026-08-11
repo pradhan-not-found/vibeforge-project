b9fb70fc05728df8dbb33798c30a47de22630944
pradhan-not-found <souradeeppradhan7@gmail.com>
2026-08-11 17:06:05 +0530

    Fix footer image URLs and adjust Aniruddha Das image position

---
+++ fix_footer_img.js
@@ -0,0 +1,14 @@
+﻿const fs = require('fs');
+let page = fs.readFileSync('src/app/page.tsx', 'utf8');
+
+page = page.replace(
+    'src="/footer/img-footer-1.avif"',
+    'src="https://cofounder.co/footer/img-footer-1.avif"'
+);
+page = page.replace(
+    'url(/footer/Holo-bg-card.png)',
+    'url(https://cofounder.co/footer/Holo-bg-card.png)'
+);
+
+fs.writeFileSync('src/app/page.tsx', page);
+console.log("Updated footer image URLs to absolute");
