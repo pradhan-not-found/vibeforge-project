00d6d640cf8b8ea0c54cf44bc91f2ac997c03325
pradhan-not-found <souradeeppradhan7@gmail.com>
2026-08-11 17:15:52 +0530

    Fix remaining missing images

---
+++ fix_remaining_images.js
@@ -0,0 +1,14 @@
+﻿const fs = require('fs');
+let page = fs.readFileSync('src/app/page.tsx', 'utf8');
+
+// Replace logos
+page = page.replace(/\/logos\//g, 'https://cofounder.co/logos/');
+
+// Replace decor
+page = page.replace(/\/decor\//g, 'https://cofounder.co/decor/');
+
+// Replace _next/image
+page = page.replace(/\/_next\/image\?url=/g, 'https://cofounder.co/_next/image?url=');
+
+fs.writeFileSync('src/app/page.tsx', page);
+console.log("Replaced remaining missing images.");
