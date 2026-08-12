21f21e195e492c86c9342fc43524ffa3c7784078
24f2006234 <aniruddha84860@gmail.com>
2026-08-12 16:06:14 +0530

    Perform code cleanup by removing temporary files

--- filter_msg.py
+++
@@ -1,5 +0,0 @@
-import sys
-lines = sys.stdin.readlines()
-content = ''.join(lines)
-content = content.replace('cofounder.co', 'the template').replace('Cofounder', 'the template').replace('cofounder', 'the template')
-sys.stdout.write(content)
--- fix_alt.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/app/page.tsx', 'utf8');
-code = code.replace(/<img alt loading/g, '<img alt="" loading');
-fs.writeFileSync('src/app/page.tsx', code);
